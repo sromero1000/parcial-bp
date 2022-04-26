2bc29b63b4e769e59b2565126db53b89ebc9a18c
= <=>
2022-04-25 20:33:09 -0500

    creacion de variables

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,7 +4,6 @@ import {firebase} from '../firebase'
 
 
 
-
 const Formulario = () => {
     const [nombres, setnombres] = React.useState('')
     const [apellidos, setapellidos] = React.useState('')
